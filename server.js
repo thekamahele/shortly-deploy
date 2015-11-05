4b9c498a9c255c012c32d7a6c52cb6f9b897fa9e
Hack Reactor Students <accounts@hackreactor.com>
2015-11-04 17:37:09 -0800

    Undoing minify

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var app = require('./server-config.js');
 
 // var port = 4568;
-
-app.listen(process.env.PORT);
+console.log('process', process.env.PORT)
+app.listen(process.env.PORT || 3000);
 
 console.log('Server now listening on port ' + process.env.PORT);
