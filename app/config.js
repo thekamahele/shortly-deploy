1eb8ced0404fd0c54828eaed12f9dc03c1b7383f
Hack Reactor Students <accounts@hackreactor.com>
2015-11-05 11:23:58 -0800

    Finished local Mongo refactor

--- app/config.js
+++ app/config.js
@@ -1,7 +1,10 @@
-var Bookshelf = require('bookshelf');
 var path = require('path');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test')
+var bcrypt = require('bcrypt-nodejs');
+var Promise = require('bluebird');
+
+
 var mdb = mongoose.connection;
 mdb.on('error', console.error.bind(console, 'connection error:'));
 mdb.once('open', function (callback) {
@@ -27,4 +30,18 @@ mondb.Urls = new mongoose.Schema({
   visits: Number
 });
 
+mondb.User.methods.hashPassword = function(){
+    var cipher = Promise.promisify(bcrypt.hash);
+    return cipher(this.password, null, null).bind(this)
+      .then(function(hash) {
+        console.log(hash);
+        this.password = hash;
+      });
+}
+
+mondb.User.methods.comparePassword = function(attemptedPassword, callback) {
+    bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
+      callback(isMatch);
+    });
+  },
 module.exports = mondb;
